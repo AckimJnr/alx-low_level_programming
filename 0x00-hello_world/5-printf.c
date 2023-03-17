b890770103942529f8695d6681341f3f936f6ba6
AckimJnr <acklongwe@gmail.com>
2023-03-17 21:34:26 +0200

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of our application
+ *
+ * Description: Function to print text using the function printf
+ * Return: Return 0 if successful else return error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

618a453e8816ad2b86778c61aafe0fad2818610d
AckimJnr <acklongwe@gmail.com>
2023-03-18 00:12:14 +0200

    Add 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @letter - stores all alphabet chars 1 at a time
+ *
+ * Description: This program prints the alphabet in lowercase
+ * Return: 0 after successful execution or else return 1
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		printf("%c", letter);
+	printf("\n");
+	return (0);
+}

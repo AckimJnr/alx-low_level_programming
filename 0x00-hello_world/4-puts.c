2b3465bf6dcb1d7d88e0086837a4a5784c863eeb
AckimJnr <acklongwe@gmail.com>
2023-03-17 21:14:27 +0200

    Add 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main function 
+ * prints message to console using puts function
+ *
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
+ 
+

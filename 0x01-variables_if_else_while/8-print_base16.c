c43eff5053a4d33923fffc21d770607b23b75c42
AckimJnr <acklongwe@gmail.com>
2023-03-18 02:47:25 +0200

    Add 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @letter - stores all alphabet chars 1 at a time
+ *
+ * Description: This program prints all the numbers of base 16
+ * Return: 0 after successful execution or else return 1
+ */
+int main(void)
+{
+	char hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+		putchar(hex);
+	for (hex = 'a'; hex <= 'f'; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}

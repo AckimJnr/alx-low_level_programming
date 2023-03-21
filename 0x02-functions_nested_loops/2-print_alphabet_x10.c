e8a43815d8b06483227618561ee8cdca8cf90d3d
AckimJnr <acklongwe@gmail.com>
2023-03-21 19:47:27 +0200

    Add 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point function to the program
+ *
+ * Description: Prints lowercase alphabet on 10 lines
+ * Return: Return 0 on successful exe or else !0
+ */
+void print_alphabet_x10(void);
+{
+	char c;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+	}
+	_putchar('\n');
+}

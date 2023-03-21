e6ce8563025dd7d4042a2fc172508642c722ca12
AckimJnr <acklongwe@gmail.com>
2023-03-21 18:43:15 +0200

    Add Readme and _putchar program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+/**
+ * main - entry point function to the program
+ *
+ * Description: This program will print
+ * putchar to console without using stdio.h file
+ * Return: Return 0 on successful exe or else !0
+ */
+int main(void)
+{
+	char *wrd = "_putchar";
+	int i;
+
+	for (i = 0; wrd[i] != '\0'; i++)
+	{
+	_putchar(wrd[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project contains code the shows my prowes in function and nested loops.

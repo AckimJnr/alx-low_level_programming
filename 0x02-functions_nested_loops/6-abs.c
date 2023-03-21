45c6f5587556ce941355cc00668bce8fe79fb743
AckimJnr <acklongwe@gmail.com>
2023-03-21 21:39:17 +0200

    Add 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - gives absolute value of a number
+ * @num: number to be checked
+ *
+ * Description: Takes any number and give you it's absolute value
+ * Return: Returns the absolute value of n
+ */
+int _abs(int num)
+{
+	if (num > 0)
+		return (num);
+	else if (num == 0)
+		(return num);
+	else
+		return (num * -1);
+}

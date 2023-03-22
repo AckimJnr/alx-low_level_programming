885107fee00f56a700f81d24caec8b01b1e31e32
AckimJnr <acklongwe@gmail.com>
2023-03-22 16:24:51 +0200

    Add 100-times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table
+ * @n: multipiler
+ *
+ * Description: Prints the times table of n
+ * Return: Returns void
+ */
+void print_times_table(int n)
+{
+	int firstnum;
+	int secondnum;
+	int product;
+
+	if (n > 15 || n < 0)
+		return;
+	for (firstnum = 0; firstnum < n; firstnum++)
+	{
+		for (secondnum = 0; secondnum < n; secondnum++)
+		{
+			product = firstnum * secondnum;
+			if (secondnum == 0)
+				_putchar(product + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
+				else if( product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product % 100)/10));
+				}
+				else
+				{
+					_putchar('0' + (product / 10));
+				}
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}

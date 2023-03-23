72254685aadf5cf52c0fa4e6d7e5dc98a1a4cc8e
AckimJnr <acklongwe@gmail.com>
2023-03-23 16:39:01 +0200

    Add 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - checks if character is uppercase or not
+ * @c: character to be checked
+ *
+ * Description: This function tests the cases of a character
+ * Return: Return 1 if c is uppercase and 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}

05fce199bd90b7b669b58ad44c83e12c69315e6e
AckimJnr <acklongwe@gmail.com>
2023-03-23 16:35:06 +0200

    Update 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 /**
- * int _isupper - checks if character is uppercase or not
+ * _isupper - checks if character is uppercase or not
  * @c: character to be checked
  *
+ * Description: This function tests the cases of a character
  * Return: Return 1 if c is uppercase and 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }

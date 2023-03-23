c67738d36b89290a2494ea41b7231ef420440a10
AckimJnr <acklongwe@gmail.com>
2023-03-23 16:46:59 +0200

    Add Mult file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - function to multiply two files
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Returns the products of the two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

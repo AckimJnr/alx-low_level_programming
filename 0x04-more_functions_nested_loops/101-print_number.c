6c07db010375974e4f66cfb50891638279644536
AckimJnr <acklongwe@gmail.com>
2023-03-24 15:23:48 +0200

    Add recursion to 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,23 +7,14 @@
  */
 void print_number(int n)
 {
-	int divisor = 1;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	while (n / divisor >= 10)
+	if (n / 10 != 0)
 	{
-		divisor *= 10;
+		print_number(n / 10);
 	}
-	while (divisor > 0)
-	{
-		int digit = n / divisor;
-
-		_putchar('0' + digit);
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar((n % 10) + 48);
 }

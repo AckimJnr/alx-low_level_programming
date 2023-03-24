9b53267c3fac1763880186068c55301d70655b40
AckimJnr <acklongwe@gmail.com>
2023-03-24 09:36:14 +0200

    Add 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line on console
+ * @n: determines the length of the line
+ *
+ * Return: void
+ */
+
+int print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			continue;
+		_putchar('_');
+	}
+	_putchar('\n');
+}

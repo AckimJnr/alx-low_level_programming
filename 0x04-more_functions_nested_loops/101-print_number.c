c2ee270eb8a415e5f1fe09585cbdd75da4d7cac9
AckimJnr <acklongwe@gmail.com>
2023-03-24 14:54:47 +0200

    Update 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,30 +8,14 @@
  */
 void print_number(int n)
 {
-	int absVal = abs(n);
-
-	if (n < 0)
+	if ( n < 0)
+	{
 		_putchar('-');
-	if (absVal >= 10 && absVal <= 99)
-	{
-		_putchar((absVal / 10) + '0');
-		_putchar((absVal % 10) + '0');
+		n = -n;
 	}
-	else if (absVal >= 100 && absVal <= 999)
+	if (n / 10 != 0)
 	{
-		_putchar((absVal / 100) + '0');
-		_putchar(((absVal / 10) % 10) + '0');
-		_putchar((absVal % 10) + '0');
+		print_number(n / 10);
 	}
-	else if (absVal >= 1000 && absVal <= 9999)
-	{
-		_putchar((absVal / 1000) + '0');
-		_putchar(((absVal / 1000) % 10) + '0');
-		_putchar(((absVal / 10) % 10) + '0');
-		_putchar((absVal % 10) + '0');
-	}
-	else
-	{
-		_putchar(absVal + '0');
-	}
+	_putchar((n % 10) + 48);
 }

14c3e4d647eda5b2a98bab8157fd2b4c416b9f07
AckimJnr <acklongwe@gmail.com>
2023-03-28 23:45:18 +0200

    Add 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,12 @@ void puts_half(char *str)
 
 	if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		for (i = (len - 1) / 2; i < len; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = (len / 2); str[i] != '\0'; i++)
+		for (i = (len / 2); i < len; i++)
 			_putchar(str[i]);
 		_putchar('\n');
 	}

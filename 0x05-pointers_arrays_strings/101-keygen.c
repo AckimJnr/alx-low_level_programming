faba408386eb44a014c46fb94e1a1b32b3a972ed
AckimJnr <acklongwe@gmail.com>
2023-03-29 03:12:27 +0200

    Add last file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+/**
+ * main - generates random passwords
+ *
+ * Return: 0 if successful 1- otherwise
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASS_LENGTH 12
+
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	char password[PASS_LENGTH + 1];
+
+	for (i = 0; i < PASS_LENGTH; i++)
+	{
+		password[i] = charset[rand() % sizeof(charset)];
+	}
+	password[PASS_LENGTH] = '\0';
+
+	printf("%s\n", password);
+	return (0);
+}

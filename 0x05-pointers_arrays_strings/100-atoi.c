7273dc0b623b34e0edc8730305fcd8364d297292
AckimJnr <acklongwe@gmail.com>
2023-03-29 02:10:08 +0200

    Add 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+/**
+ * _atoi - convert string to interger
+ * @s: string to be converted
+ *
+ * Return: int
+ */
+#include <ctype.h>
+#include <limits.h>
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int num = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-' && !num)
+			sign = -1;
+		else if (isdigit(*s))
+		{
+			result = result * 10 + (*s - '0');
+			num = 1;
+		}
+		else if (num)
+		{
+			break;
+		}
+		s++;
+	}
+	return (result * sign);
+}
+

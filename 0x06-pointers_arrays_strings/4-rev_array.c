2f83b0350839a941eea39dabc78fa2be8a9c0599
AckimJnr <acklongwe@gmail.com>
2023-03-30 16:55:07 +0200

    Rev int chars

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+/**
+ * reverse_array - reverse a given array
+ * @a: the array to reverse
+ * @n: the size of the array
+ *
+ * Return: returns void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	for ( i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}

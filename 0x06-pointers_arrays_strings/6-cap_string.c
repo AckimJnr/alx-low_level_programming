8ed0a4dfa99fc171477313f9288af27e6483df9b
AckimJnr <acklongwe@gmail.com>
2023-03-30 19:19:31 +0200

    Add 6-cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+/**
+ *cap_string - function to captalise every word of a string
+ *@str: string to be processed
+ *
+ *Return: Return char
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		if (str[i] == 32 || str[i] == 10 || str[i] == 9)
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}

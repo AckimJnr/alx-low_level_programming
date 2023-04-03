2626c96da775360f484f90c92baed8c1464c5078
AckimJnr <acklongwe@gmail.com>
2023-04-03 18:21:37 +0200

    Search a char from in a string of text

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/**
+ * _strchr - search a string and return the pointer to first occurence
+ * @s: string to be searched
+ * @c: search char
+ *
+ * Return: Return pointer of the firt occurence of the item
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if ( *s == c)
+			return (char*) s;
+		s++;
+	}
+	if (c == '\0')
+		return (char*) s;
+	
+	return NULL;
+}

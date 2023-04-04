0ae9302026f8d58966edd07c563fd6ed319b62a1
AckimJnr <acklongwe@gmail.com>
2023-04-04 09:37:55 +0200

    Update file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - sets pointer to char
+ * @s: string tobe overwritten
+ * @to: char to set in the memory location of **s
+ *
+ * Return: Returns void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

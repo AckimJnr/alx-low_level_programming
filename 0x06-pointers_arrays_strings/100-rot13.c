bfeba33b601984e5d08d0953e3e048cacbd3fdfc
AckimJnr <acklongwe@gmail.com>
2023-04-04 07:10:58 +0200

    Cypher by ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <string.h>
+/**
+ * rot13 - A Basic encryption algorithm
+ * @str: string to be encrypted
+ *
+ * Return: return a pointer to the encrypted text
+ */
+
+char *rot13(char *str)
+{
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	int j;
+	/** int len = strlen(str);*/
+	int alphlen = strlen(output);
+
+	while ( *(str + i) != '\0')
+	{
+		for (j = 0; j < alphlen; j++)
+		{
+			if (*(str + i) == *(input + j))
+				*(str + i) = *(output + j);
+		}
+		i++;
+	}
+	return (str);
+}
+

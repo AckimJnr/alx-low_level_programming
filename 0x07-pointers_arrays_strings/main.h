fb84bede9ae899aa0bd9e361a9646a3fc7a000a2
AckimJnr <acklongwe@gmail.com>
2023-04-04 09:35:53 +0200

    Add setstring function prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int comp_strns(char *firstStr, char *secStr, int n);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 #endif

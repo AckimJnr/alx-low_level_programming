701837787ee61beea7ee04ee503fb305d10b0d1b
AckimJnr <acklongwe@gmail.com>
2023-04-05 23:36:29 +0200

    Determine if a number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+/**
+ * is_prime_number - checks if number is prime
+ * @n: interger to be checked
+ *
+ * Return: return 1 if interger is prime else 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1 || n == -1)
+		return (0);
+	else if (n == 2 || n == -2)
+		return (1);
+	else if (n == 3 || n == -3)
+		return (1);
+	else if (n == 5 || n == -5)
+		return (1);
+	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+		return (0);
+	else
+		return (1);
+}

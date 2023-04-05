d8e6caf041ff28688d948efd305a73754d7eda3e
AckimJnr <acklongwe@gmail.com>
2023-04-05 22:53:48 +0200

    Calculate power of a number using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - find power of a number using recursion
+ * @x: base
+ * @y: power
+ *
+ * Return: x to the power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+
+	return (x * _pow_recursion(x, y - 1));
+}

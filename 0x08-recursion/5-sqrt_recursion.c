b54d4276c04fe12f29b5da87e00e3e0b0912b803
AckimJnr <acklongwe@gmail.com>
2023-04-06 01:43:16 +0200

    Calcurate sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+int refine_sqrt(int n, int res);
+/**
+ * _sqrt_recursion - find square root of a number using recursion
+ * @n: base
+ *
+ * Return: x to the power y
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (refine_sqrt(n, n));
+}
+
+/**
+ * refine_sqrt - helps sqrt computation
+ * @n: number to be calcurated
+ * @res: result projection
+ * Return: return -1 if n does not have a natural sqrt
+ */
+
+int refine_sqrt(int n, int res)
+{
+	int dif = (res * res - n) / (2 * res);
+
+	if (res < 1)
+		return (-1);
+
+	if (dif < 0)
+		dif -= dif;
+	if (dif == 0)
+		return (res);
+	return (refine_sqrt(n, (res + n / res) / 2));
+}

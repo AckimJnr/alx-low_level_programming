bea9f3f24df0c3057a714642a624b4262979ec6e
AckimJnr <acklongwe@gmail.com>
2023-04-05 23:44:06 +0200

    Determine if a number is prime or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,7 +16,7 @@ int is_prime_number(int n)
 	else if (n == 5 || n == -5)
 		return (1);
 	else if (n == 7 || n == -7)
-		return (1);
+		return (0);
 	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
 		return (0);
 	else

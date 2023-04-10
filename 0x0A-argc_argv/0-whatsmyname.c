615607d6221146f0ac7003002177fe9ecc255782
AckimJnr <acklongwe@gmail.com>
2023-04-10 21:33:22 +0200

    Add 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * @argc: number of arguments passed
+ * @argv: array of string arguments
+ *
+ * Return: Return 0 on successful execution others !0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

7d90acd83926d80ee8b7de4dc5ed0a0fbf39be67
AckimJnr <acklongwe@gmail.com>
2023-04-12 01:59:14 +0200

    Update grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,19 @@ int **alloc_grid(int width, int height)
 	arr = (int **) malloc(height * sizeof(int *));
 
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 
 	for (i = 0; i < width; i++)
 	{
 		arr[i] = (int *) malloc(width * sizeof(int));
 		if (arr[i] == NULL)
+		{
+			free(arr[i]);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{

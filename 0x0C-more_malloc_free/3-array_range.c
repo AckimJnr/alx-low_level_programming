3529f0218cc26ff356058f96673605855a20a6eb
AckimJnr <acklongwe@gmail.com>
2023-04-14 22:18:18 +0200

    Add array range file

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * array_range - creates an array of a range of sorted numbers
+ * @min: minmum number in the range
+ * @max: maximum number in the range
+ *
+ * Return: Returns a pointer to location containing the arranged numbers
+ * if min > max or if malloc fails, it Returns NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * (max + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= max; i++)
+	{
+		ptr[i] = min++;
+	}
+	return (ptr);
+}

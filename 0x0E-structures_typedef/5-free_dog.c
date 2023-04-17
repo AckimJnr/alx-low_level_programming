695a2a24eca2501ab7e4c0838b3d06f773e6d163
AckimJnr <acklongwe@gmail.com>
2023-04-18 00:03:08 +0200

    Add free dog file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees the memory allocated for dog
+ * @d: the dog to be freed
+ *
+ * Return: Returns void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}

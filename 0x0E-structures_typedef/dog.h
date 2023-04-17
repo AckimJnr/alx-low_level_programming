a14efa2e5edb85fa6f7e3333d4384c5f3ff45c7a
AckimJnr <acklongwe@gmail.com>
2023-04-17 21:41:34 +0200

    Add structures and typedefs project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a type of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This will create a composite type go dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

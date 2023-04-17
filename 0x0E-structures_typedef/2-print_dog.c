137db45b1c0361f84f9cb902c45a32583c58e546
AckimJnr <acklongwe@gmail.com>
2023-04-17 22:28:14 +0200

    Add printing of dog properties

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints the struct dog
+ * @d: dog to be printed
+ *
+ * Return: the function returns void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("%p", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("%p", d->owner);
+	}
+	printf("\n");
+}

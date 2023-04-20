7c69827921b0de885c3e0ef242310f48af9aa220
AckimJnr <acklongwe@gmail.com>
2023-04-20 15:23:46 +0200

    Update print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,18 @@
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * print_strings - prints strings
+ * @separator: seperator for strings
+ * @n: number of string to be printed
+ *
+ * Return: Returns nothing
+ */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
-	
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)

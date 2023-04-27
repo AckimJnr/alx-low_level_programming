ce287586eb2167d0d724f643d7e27bebc779a033
AckimJnr <acklongwe@gmail.com>
2023-04-28 00:11:43 +0200

    Update free list function

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,5 +8,13 @@
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current_node = head;
+	list_t *next_node;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
 }

622cbb5827132a911240824fe6e701acc2896331
AckimJnr <acklongwe@gmail.com>
2023-04-27 20:42:46 +0200

    Add list len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * list_len - Prints elements of a list
+ * @h: list struct to be printed
+ *
+ * Return: Returns the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+
+	size_t number_of_nodes = 0;
+
+
+	if (h == NULL)
+		return (0);
+
+	temp = h;
+	while (temp != NULL)
+	{
+		number_of_nodes++;
+		temp = temp->next;
+	}
+	return (number_of_nodes);
+}

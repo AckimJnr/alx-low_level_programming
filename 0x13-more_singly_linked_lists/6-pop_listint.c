5e8d286f17aae78944954bf506df2009fa9ce059
AckimJnr <acklongwe@gmail.com>
2023-05-02 18:25:07 +0200

    Add delete node function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - delets the first item on the list
+ * @head: head of the list
+ *
+ * Return: returns the data on the head node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int head_data = 0;
+
+	if (head == NULL)
+		return (head_data);
+	temp = (*head);
+	(*head) = (*head)->next;
+	head_data = temp->n;
+
+	free(temp);
+
+	return (head_data);
+}

51fa852ede7dae0a69745bcc345400334e7ee42a
AckimJnr <acklongwe@gmail.com>
2023-05-03 22:06:13 +0200

    Update print lists safely function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "lists.h"
 /**
  * print_listint_safe - prints a list safely
@@ -9,25 +10,32 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_number = 0;
-	const listint_t *current = head, *next_node;
+	size_t node_number = 0, i, count = 0;
+	const listint_t **visited_node = NULL;
 
-	if (head == NULL)
-		return (node_number);
+	visited_node = malloc(sizeof(listint_t *) * 200);
 
-	while (current)
+	if (visited_node == NULL)
+		exit(98);
+
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		node_number++;
-		next_node = current->next;
-
-		if (next_node >= current)
+		for (i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)next_node, next_node->n);
-			break;
+			if (visited_node[i] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				node_number++;
+				free(visited_node);
+				return (node_number);
+			}
 		}
-		current = next_node;
+		printf("[%p] %d\n", (void *)head, head->n);
+		visited_node[count] = head;
+		count++;
+		head = head->next;
 	}
+	free(visited_node);
 
 	return (node_number);
 }

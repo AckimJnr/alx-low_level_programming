83cbd79e78ab3302c634dfc913e3640139eaa0ee
AckimJnr <acklongwe@gmail.com>
2023-05-11 18:35:13 +0200

    Add check write return

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
 	while ((nbytes = read(source_file_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (write(replica_fd, buffer, nbytes) != nbytes)
+		if (write(replica_fd, buffer, nbytes) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);

8458966018f8d4f62c91cce83e1887e9c98894a8
AckimJnr <acklongwe@gmail.com>
2023-05-12 11:52:53 +0200

    remove written bytes check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -54,19 +54,15 @@ int main(int argc, char *argv[])
  */
 void copy_file(int source_file_fd, int replica_fd, char buffer[], char *argv[])
 {
-	int nbytes, write_fd, bytes_written = 0;
+	int nbytes, write_fd;
 
 	while ((nbytes = read(source_file_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		while (bytes_written < nbytes)
+		write_fd = write(replica_fd, buffer, nbytes);
+		if (write_fd == -1)
 		{
-			write_fd = write(replica_fd, buffer, nbytes);
-			if (write_fd == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			bytes_written += write_fd;
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 	if (nbytes == -1)

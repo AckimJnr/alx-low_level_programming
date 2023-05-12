ff1ec22c390bb3d2d2d57037f5ea6e4299a9a71a
AckimJnr <acklongwe@gmail.com>
2023-05-12 11:36:44 +0200

    Update copy program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,9 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include "main.h"
 #define BUFFER_SIZE 1024
-
+void copy_file(int, int, char buffer[], char *argv[]);
 /**
  * main - a c program to copy the contents of one file to another
  * @argc: arguments count
@@ -43,4 +42,43 @@ int main(int argc, char *argv[])
 
 	return (0);
 
+}
+/**
+ * copy_file - copies the contents of a file to another file
+ * @source_file_fd: file descriptor of the source file
+ * @replica_fd: fd of the copied file
+ * @buffer: buffer size
+ * @argv: arguments passed
+ *
+ * Return: Returns void
+ */
+void copy_file(int source_file_fd, int replica_fd, char buffer[], char *argv[])
+{
+	int nbytes, write_fd;
+
+	while ((nbytes = read(source_file_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_fd = write(replica_fd, buffer, nbytes);
+		if (write_fd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (nbytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	if (close(source_file_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_file_fd);
+		exit(100);
+	}
+	if (close(replica_fd) == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", replica_fd);
+		exit(100);
+	}
 }

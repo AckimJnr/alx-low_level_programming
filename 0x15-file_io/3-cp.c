e06ad44c8518c50e50ed52b76f91fc3f7ce5d009
AckimJnr <acklongwe@gmail.com>
2023-05-13 05:11:03 +0200

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,7 +67,7 @@ void copy_file(int source_file_fd, int replica_fd, char buffer[], char *argv[])
 	}
 	if (nbytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(99);
 	}
 

353d1bdbe9ac1491a40f4e8ef22e55ea0a419498
AckimJnr <acklongwe@gmail.com>
2023-06-15 23:02:35 +0200

    Update list len function

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 /**
  * dlistint_len - prints the number of elements in a list
- * @h: heaa of the list
+ * @h: head of the list
  *
  * Return: Returns the number of elements in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *temp;
+	const dlistint_t *temp = h;
 	size_t number_of_nodes = 0;
 
 	if (h == NULL)
 		return (0);
-	temp = h;
 	while (temp != NULL)
 	{
 		number_of_nodes += 1;

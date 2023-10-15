3feb2b6ea948764dd8ea753a89648aceb0e3e916
AckimJnr <acklongwe@gmail.com>
2023-10-15 03:49:00 +0200

    Update 102 interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,6 +13,9 @@ int interpolation_search(int *array, size_t size, int value)
 	int high = size - 1;
 	int mid = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while ((array[high]) != array[low]
 			&& (value >= array[low])
 			&& (value <= array[high]))
